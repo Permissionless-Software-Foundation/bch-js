fbc3ce8b430a4ea807ea2921990a2c1bbab423ef
TRBC <therealbitcoin.club@protonmail.com>
2020-11-09 14:42:39 -0400

    make issue #10 unit tests run

--- test/unit/fixtures/ninsight-mock.js
+++ test/unit/fixtures/ninsight-mock.js
@@ -2,6 +2,47 @@
   Mocking data for unit tests for the Ninsight library.
 */
 
+const details = {
+      'address': 'mkPvAKZ2rar6qeG3KjBtJHHMSP1wFZH7Er',
+      'satoshis': 2782729129,
+      'height': 534105,
+      'confirmations': 123,
+      'timestamp': 1441068774,
+      'fees': 35436,
+      'outputIndexes': [
+        0
+      ],
+      'inputIndexes': [],
+      'tx': {
+        'hash': 'bb0ec3b96209fac9529570ea6f83a86af2cceedde4aaf2bfcc4796680d23f1c7',
+        'version': 1,
+        'inputs': [
+          {
+            'prevTxId': 'ea5e5bafbf29cdf6f6097ab344128477e67889d4d6203cb43594836daa6cc425',
+            'outputIndex': 1,
+            'sequenceNumber': 4294967294,
+            'script': '483045022100f4d169783bef70e3943d2a617cce55d9fe4e33fc6f9880b8277265e2f619a97002201238648abcdf52960500664e969046d41755f7fc371971ebc78002fc418465a6012103acdcd31d51272403ce0829447e59e2ac9e08ed0bf92011cbf7420addf24534e6',
+            'scriptString': '72 0x3045022100f4d169783bef70e3943d2a617cce55d9fe4e33fc6f9880b8277265e2f619a97002201238648abcdf52960500664e969046d41755f7fc371971ebc78002fc418465a601 33 0x03acdcd31d51272403ce0829447e59e2ac9e08ed0bf92011cbf7420addf24534e6',
+            'output': {
+              'satoshis': 2796764565,
+              'script': '76a91488b1fe8aec5ae4358a11447a2f22b2781faedb9b88ac'
+            }
+          }
+        ],
+        'outputs': [
+          {
+            'satoshis': 2782729129,
+            'script': '76a9143583efb5e64a4668c6c54bb5fcc30af4417b4f2d88ac'
+          },
+          {
+            'satoshis': 14000000,
+            'script': '76a9149713201957f42379e574d7c70d506ee49c2c8ad688ac'
+          }
+        ],
+        'nLockTime': 534089
+      }
+    }
+
 const utxo = {
   utxos: [
     {
@@ -22,8 +63,11 @@ const utxo = {
 }
 
 const utxoPost = [utxo, utxo]
+const detailsPost = [details, details]
 
 module.exports = {
+  details,
+  detailsPost,
   utxo,
   utxoPost
 }
--- test/unit/ninsight.js
+++ test/unit/ninsight.js
@@ -78,4 +78,82 @@ describe(`#Ninsight`, () => {
       assert.property(result[0], "asm")
     })
   })
+
+
+  describe(`#details`, () => {
+    it(`should throw an error for improper input`, async () => {
+      try {
+        const addr = 12345
+
+        await bchjs.Ninsight.details(addr)
+        assert.equal(true, false, "Unexpected result!")
+      } catch (err) {
+        //console.log(`err: `, err)
+        assert.include(
+          err.message,
+          `Input address must be a string or array of strings.`
+        )
+      }
+    })
+
+    it(`should GET details for a single address`, async () => {
+      // Stub the network call.
+      sandbox.stub(axios, "post").resolves({ data: mockData.details })
+
+      const addr = "mkPvAKZ2rar6qeG3KjBtJHHMSP1wFZH7Er"
+
+      const result = await bchjs.Ninsight.details(addr)
+      console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.property(result, "satoshis")
+      assert.property(result, "height")
+      assert.property(result, "confirmations")
+      assert.property(result, "timestamp")
+      assert.property(result, "fees")
+      assert.property(result, "outputIndexes")
+      assert.property(result, "inputIndexes")
+      assert.property(result, "tx")
+    })
+
+    it(`should GET details for an array of addresses`, async () => {
+      // Mock the network call.
+      sandbox.stub(axios, "post").resolves({ data: mockData.detailsPost })
+
+      const addr = [
+        "bitcoincash:qp3sn6vlwz28ntmf3wmyra7jqttfx7z6zgtkygjhc7",
+        "bitcoincash:qz0us0z6ucpqt07jgpad0shgh7xmwxyr3ynlcsq0wr"
+      ]
+
+      const result = await bchjs.Ninsight.utxo(addr)
+      // console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+      assert.isArray(result)
+
+      assert.property(result[0], "satoshis")
+      assert.property(result[0], "height")
+      assert.property(result[0], "confirmations")
+      assert.property(result[0], "timestamp")
+      assert.property(result[0], "fees")
+      assert.property(result[0], "outputIndexes")
+      assert.property(result[0], "inputIndexes")
+      assert.property(result[0], "tx")
+
+        /*
+        I would like to refactor this test according to the DRY principle
+
+        assertDetails(result[0])
+
+        assertDetails(data) {
+              assert.property(data, "satoshis")
+              assert.property(data, "height")
+              assert.property(data, "confirmations")
+              assert.property(data, "timestamp")
+              assert.property(data, "fees")
+              assert.property(data, "outputIndexes")
+              assert.property(data, "inputIndexes")
+              assert.property(data, "tx")
+        }
+        */
+    })
+  })
 })
