faa297246011494826e3d7d52e40a7783125c71d
Chris Troutner <chris.troutner@gmail.com>
2021-03-24 07:28:38 -0700

    fix(Transaction.get()): Added documentation

--- src/transaction.js
+++ src/transaction.js
@@ -12,8 +12,28 @@ class Transaction {
     this.rawTransaction = new RawTransaction(config)
   }
 
-  // Get hydrated details about a transaction, including SLP token details if
-  // it's an SLP transaction.
+  /**
+   * @api Transaction.get() get()
+   * @apiName get
+   * @apiGroup Transaction
+   * @apiDescription
+   * Returns an object of transaction data, including addresses for input UTXOs.
+   * If it is a SLP token transaction, the token information for inputs and
+   * outputs will also be included.
+   *
+   * This is an API heavy call. This function will only work with a single txid.
+   * It does not yet support an array of TXIDs.
+   *
+   * @apiExample Example usage:
+   * (async () => {
+   * try {
+   *  let txData = await bchjs.Transaction.get("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098");
+   *  console.log(txData);
+   * } catch(error) {
+   * console.error(error)
+   * }
+   * })()
+   */
   async get (txid) {
     try {
       if (typeof txid !== 'string') {
