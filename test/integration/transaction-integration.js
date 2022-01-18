656b0f6809fba42fc70c7374a1e3bb6e9a0f0d16
Chris Troutner <chris.troutner@gmail.com>
2022-01-17 19:12:12 -0800

    feat(Transaction.get()): Replacing call with proxy to psf-slp-indexer

--- test/integration/transaction-integration.js
+++ test/integration/transaction-integration.js
@@ -11,12 +11,12 @@ describe('#Transaction', () => {
     if (process.env.IS_USING_FREE_TIER) await bchjs.Util.sleep(1000)
   })
 
-  describe('#get', () => {
+  describe('#getOld', () => {
     it('should get details about a non-SLP transaction', async () => {
       const txid =
         '2b37bdb3b63dd0bca720437754a36671431a950e684b64c44ea910ea9d5297c7'
 
-      const result = await bchjs.Transaction.get(txid)
+      const result = await bchjs.Transaction.getOld(txid)
       // console.log(`result: ${JSON.stringify(result, null, 2)}`)
 
       // Assert that there are stanardized properties.
@@ -37,7 +37,7 @@ describe('#Transaction', () => {
       const txid =
         '266844d53e46bbd7dd37134688dffea6e54d944edff27a0add63dd0908839bc1'
 
-      const result = await bchjs.Transaction.get(txid)
+      const result = await bchjs.Transaction.getOld(txid)
       // console.log(`result: ${JSON.stringify(result, null, 2)}`)
 
       // Assert that there are stanardized properties.
@@ -69,9 +69,10 @@ describe('#Transaction', () => {
     // that is also a valid SLP tx, but is unrelated. Both TXs pass DAG validation,
     // but for separate tokens.
     it('should get problematic transaction', async () => {
-      const txid = 'a19f2f395a8b0e15b6202944c56834367d128f1e3630486a4756de53424a46fe'
+      const txid =
+        'a19f2f395a8b0e15b6202944c56834367d128f1e3630486a4756de53424a46fe'
 
-      const result = await bchjs.Transaction.get(txid)
+      const result = await bchjs.Transaction.getOld(txid)
       // console.log(`result: ${JSON.stringify(result, null, 2)}`)
 
       // The token ID should equal the txid for this Vin.
